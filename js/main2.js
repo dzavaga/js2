e2e1b82c2b9ef9629cb31de01a2dcd86b9d6fcf1
dzavaga <mishaofis86@gmail.com>
2023-04-18 13:20:08 +0000

    code

--- js/main2.js
+++ js/main2.js
@@ -32,6 +32,36 @@ let age = +prompt("Введіть вік:");
 let isAdult = age >= 18 ? "Вітаю, ви повнолітній!" : "Ви ще надто молоді";
 console.log(isAdult);
 /*4 завдання*/
+let arr = [4, 5, 2, 1, 6, 5, 3, 5, 2, 5];
+
+// Створити об'єкт, щоб порахувати кількість входжень кожного числа
+let counts = {};
+for (let i = 0; i < arr.length; i++) {
+  if (counts[arr[i]] === undefined) {
+    counts[arr[i]] = 1;
+  } else {
+    counts[arr[i]]++;
+  }
+}
+
+// Знайти число з найбільшою кількістю входжень
+let maxCount = 0;
+let data;
+for (let key in counts) {
+  if (counts[key] > maxCount) {
+    maxCount = counts[key];
+    data = key;
+  }
+}
+
+// Додати число до нового масиву
+let newArr = [data];
+
+// Видалити всі входження числа з поточного масиву
+arr = arr.filter(num => num !== Number(data));
+
+console.log(newArr); // [5]
+console.log(arr); // [4, 2, 1, 6, 3, 2]
 
 
 
